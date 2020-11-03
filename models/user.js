8b4b3023e1b2fe35ae9dbcc4820486ffadb60d13
Rahul Hooda <rh740099@gmail.com>
2020-11-04 00:11:59 +0530

    random commit

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name:{
+        type: String,
+        maxlength: 50
+    },
+    email:{
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    password:{
+        type:String,
+        minlength: 8
+    },
+    lastname:{
+        type : String,
+        maxlength: 50
+    },
+    token:{
+        type: String,
+    },
+    tokenExp:{
+        type: Number
+    }
+});
+
+const User = mongoose.model('User',userSchema);
+
+module.exports ={ User }
