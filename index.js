11d6ec820deaef1306c22b5f781e39da2a3f49cf
Rahul Hooda <rh740099@gmail.com>
2020-11-26 21:20:38 +0530

    another commit

--- index.js
+++ index.js
@@ -4,30 +4,82 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { User } =require('./models/user');
-
+const { auth } = require('./midleware/auth');
 const config = require('./config/keys');
+const user = require('./models/user');
 mongoose.connect(config.mongoURI,
-{useNewUrlParser: true},{ useUnifiedTopology: true }).then( () => console.log('DB connected'))
+{useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex: true }).then( () => console.log('DB connected'))
                                                      .catch(err => console.error(err));
 
-                                                    
+
+
+                                                     
+
+ app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+
 app.get('/',(req,res)=>{
     res.json({"hello" : "world"});
 })
+app.get('/api/user/auth',auth, (req,res)=>{
+    res.status(200).json({
+        _id: req._id,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role
+    })
+})
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-app.use(cookieParser());
 
 app.post('/api/users/register', (req,res)=>{
 
     const user = new User(req.body)
-    user.save((err, userData)=> {
-        if(err) return res.json({success: false, err })
-            
-    })
-    return res.status(200).json({success: true})
     
+    user.save((err, doc)=> {
+        if(err) return res.json({success: false, err });
+            
+    
+    return res.status(200).json({success: true,userData : doc})
 })
-// 
+});
+app.post("/api/user/login", (req,res)=>{
+    console.log(req.body);
+    User.findOne( {email:req.body.email} , (err,user)=>{
+        
+        if(!user) return res.json({
+                loginSuccess : false,
+                message : "Auth failed, email not found" 
+        })
+    
+        
+    user.comparePassword( req.body.password, (err,isMatch)=>{
+         console.log(isMatch);
+        if(!isMatch)
+            return res.json({
+                loginSuccess : false,
+                message : "wrong password" 
+            })
+        
+    });
+    user.generateToken((err,user)=>{
+        if(err) return res.status(400).send(err);
+        res.cookie("x_auth", user.token)
+           .status(200)
+           .json({loginSuccess:true})
+    })
+
+})
+})
+
+app.post("/api/user/logout",auth,(req,res)=>{
+    User.findOneAndUpdate({_id : req.user._id},{token:""},(err,doc)=>{
+        if(err) return res.json({success:false ,err})
+        return res.status(200).send({success:true})
+    })
+})
 app.listen(5000);
